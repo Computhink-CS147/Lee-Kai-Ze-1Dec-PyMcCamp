88b2d056fe7cdef45b75acf2e15b044ec6402675
Computhink-CS147 <cs147@computhink.com.sg>
2025-12-01 08:49:32 +0000

    Dec 1, 2025, 8:49 AM

--- Day1Folder/minecraft_day1.py
+++ Day1Folder/minecraft_day1.py
@@ -4,3 +4,51 @@
 
 
 ################## On Chat Commands Section #####################
+def move_forward(steps):
+    agent.move(FORWARD, steps)
+
+def move_backward(steps):
+    agent.move(BACK, steps)
+
+def move_right(steps):
+    agent.move(RIGHT, steps)
+def move_left(steps):
+    agent.move(LEFT, steps)
+
+def turn_right():
+    agent.turn(TurnDirection.LEFT)
+
+def turn_left():
+    agent.turn(TurnDirection.RIGHT)
+
+def teleport():
+    agent.teleport_to_player()
+
+player.on_chat("come",teleport)
+player.on_chat("forward",move_forward)
+player.on_chat("back",move_backward)
+player.on_chat("left",move_left)
+player.on_chat("right",move_right)
+player.on_chat("turn left",turn_left)
+player.on_chat("turn right", turn_right)
+def cow():
+    mobs.spawn(COW, pos(423, 76, -1767))
+player.on_chat("cow",cow)
+def obby():
+    agent.move(FORWARD, 4)
+    agent.move(LEFT, 4)
+    agent.move(FORWARD, 3)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
