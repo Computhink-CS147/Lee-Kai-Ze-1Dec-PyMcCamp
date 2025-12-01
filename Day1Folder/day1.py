12449dd4eca8cf8b699f3a1d3122dcb1efd93bc4
Computhink-CS147 <cs147@computhink.com.sg>
2025-12-01 07:29:31 +0000

    Dec 1, 2025, 7:29 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,9 +1,9 @@
 def addition(num1,num2):
     print(num1+num2)
-total_1addition(3,4)
+total_1=addition(3,4)
 def multiply(num1,num2):
     print(num1*num2)
-multiply(3,4)
+total2=multiply(3,4)
 
 
 
